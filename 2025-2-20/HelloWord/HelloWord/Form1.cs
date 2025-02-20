3ae8bd2b384d9830e8d4b6e125a2c64fa9782d4b
a113221059 <a113221059@mail.shu.edu.tw>
2025-02-20 11:50:53 +0800

    2025-2-20

---
+++ 2025-2-20/HelloWord/HelloWord/Form1.cs
@@ -0,0 +1,20 @@
+namespace HelloWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            abel1.Text = "好想回家";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
