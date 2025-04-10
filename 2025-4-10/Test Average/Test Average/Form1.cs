e77e37fdc0edfe38b080944c588fb4707e498bf6
a113221059 <a113221059@mail.shu.edu.tw>
2025-04-10 20:01:54 +0800

    2025-4-10

--- 2025-4-10/Test Average/Test Average/Form1.cs
+++ 2025-4-10/Test Average/Test Average/Form1.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
 
 namespace Test_Average
 {
@@ -17,42 +17,88 @@ namespace Test_Average
         {
             InitializeComponent();
         }
-
-        // The Average method accepts an int array argument
-        // and returns the Average of the values in the array.
-        private   Average(   )
+        private double Average(int[] scores)
         {
-            
+            int total = 0;
+            foreach (int score in scores)
+            {
+                total += score;
+            }
+            return (double)total / scores.Length;
         }
 
-        // The Highest method accepts an int array argument
-        // and returns the highest value in that array.
-        private   Highest(    )
+        // Highest 方法接受一個 int 陣列參數
+        // 並返回該陣列中的最高值。
+        private int Highest(int[] scores)
         {
-           
+            int highest = scores[0];
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > highest)
+                {
+                    highest = scores[i];
+                }
+            }
+            return highest;
         }
 
-        // The Lowest method accepts an int array argument
-        // and returns the lowest value in that array.
-        private   Lowest(   )
+        // Lowest 方法接受一個 int 陣列參數
+        // 並返回該陣列中的最低值。
+        private int Lowest(int[] scores)
         {
-           
+            int lowest = scores[0];
+            foreach (int score in scores)
+            {
+                if (score < lowest)
+                {
+                    lowest = score;
+                }
+            }
+            return lowest;
         }
 
         private void getScoresButton_Click(object sender, EventArgs e)
         {
-           
+            const int SIZE = 48;
+            int[] testscores = new int[SIZE];
+            int index = 0;
+            int highestScore = 0;
+            int lowestScore = 0;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+            try
+            {
+                if (OpenFlags.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(OpenFileDialog1.FileName);
+                    while (!inputFile.EndOfStream && index < SIZE)
+                    {
+                        testscores[index] = Convert.ToInt32(inputFile.ReadLine());
+                        index++;
+                    }
+                    inputFile.Close();
+                }
+                averageScore = Average(testscores);
+                highestScore = Highest(testscores);
+                lowestScore = Lowest(testscores);
+
+                averageScoreLabel.Text = averageScore.ToString("n1");
+                highScoreLabel.Text = highestScore.ToString();
+                lowScoreLabel.Text = lowestScore.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            // Close the form.
+            // 關閉表單。
             this.Close();
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
         }
     }
 }
+
+
